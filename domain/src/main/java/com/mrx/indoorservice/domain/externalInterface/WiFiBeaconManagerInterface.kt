377cea4c2eb58fda6984da0cc5012d2154cea863
Vlad Sizyh <sizyh06@gmail.com>
2023-04-16 17:47:41 +0800

    WiFiBeaconManagerInterface ready [unstable]

--- domain/src/main/java/com/mrx/indoorservice/domain/externalInterface/WiFiBeaconManagerInterface.kt
+++ domain/src/main/java/com/mrx/indoorservice/domain/externalInterface/WiFiBeaconManagerInterface.kt
@@ -2,5 +2,31 @@ package com.mrx.indoorservice.domain.externalInterface
 
 interface WiFiBeaconManagerInterface {
 
+    /* methods for config */
 
+    fun setScanInterval()   // config
+
+    /* methods for scanning all wifi points */
+
+    fun startAllScanning()
+
+    fun getAllScanLastResult()
+
+    fun getAllScanViewModel()
+
+    fun stopAllScanning()
+
+    /* methods for scanning specific wifi points */
+
+    fun setSSIDForSpecificScan(ssid: String)    // config
+
+    fun setSSIDForSpecificScan(ssid: Collection<String>)    // config
+
+    fun startSpecificScanning()
+
+    fun getSpecificScanLastResult()
+
+    fun getSpecificScanViewModel()
+
+    fun stopSpecificScanning()
 }
